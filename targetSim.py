59ebb8b4c7294c2b8c012a34ade8d81770bff9a5
Oskar Asplin <oskarba@stud.ntnu.no>
2017-10-10 19:13:42 +0200

    Functioning PDA tracking in python

--- targetSim.py
+++ targetSim.py
@@ -2,6 +2,7 @@ from sys import stdin
 import numpy as np
 import math
 import random
+import matplotlib.pyplot as plt
 
 # Global constants
 clut_h = 0.0001
@@ -10,7 +11,8 @@ clut_l = 0.00001
 # Initialized target
 x0 = np.array([[-300], [4], [-200], [5]])
 cov0 = np.diag([10, 0.5, 10, 0.5])
-x0_est = np.matmul(np.linalg.cholesky(cov0),np.random.normal(size=[4,1]))
+x0_est = x0 + np.dot(np.linalg.cholesky(cov0),np.random.normal(size=[4,1]))
+v_max = 25
 
 # Time for simulation
 dt = 1
@@ -29,8 +31,8 @@ F = np.array([[1, dt, 0, 0], [0, 1, 0, 0], [0, 0, 1, dt], [0, 0, 0, 1]])
 H = np.array([[1, 0, 0, 0], [0, 0, 1, 0]])
 R = r*np.identity(2)
 G = np.array([[dt**2/2], [dt]])
-Q = q*np.append(np.append(G*np.transpose(G), np.zeros([2,2]),axis=1),
-    np.append(np.zeros([2,2]), G*np.transpose(G),axis=1),axis=0)
+Q = q*np.append(np.append(G*G.T, np.zeros([2,2]),axis=1),
+    np.append(np.zeros([2,2]), G*G.T,axis=1),axis=0)
 K = len(time)
 gamma_g = 9.21               # Gate threshold
 c = 2                        # Normalization constant
@@ -48,9 +50,7 @@ cov_posterior = np.zeros([4,4,K])
 
 # Measurement vectors
 z_true = np.zeros([2, K])
-z = np.zeros([2, K])
-#z_gate = []
-
+z_gate = np.empty((2,0))
 
 # -----------------------------------------------
 
@@ -65,36 +65,38 @@ def pol2cart(rho, phi):
     return(x, y)
 
 def randomize_direction(x):
-    theta, rho = cart2pol(x[1],x[3])
+    rho, theta = cart2pol(x[1],x[3])
     varTheta = 0.15
     theta = theta -varTheta+random.random()*2*varTheta
-    x1,y1 = pol2cart(theta,rho)
+    x1,y1 = pol2cart(rho,theta)
     return np.array([[x[0]], [x1], [x[2]], [y1]])
 
 # -----------------------------------------------
+random.seed(a=None)
+
 # Main loop
 for k in range(K):
     # Dynamic model
     if k == 0:
-        x_true[:,k] = np.transpose(x0)
-        x_est_prior[:,k] = np.transpose(x0_est)
+        x_true[:,k] = x0.T
+        x_est_prior[:,k] = x0_est.T
         cov_prior[:,:,k] = cov0
     else:
-        x_true[:,k] = np.matmul(F,x_true[:,k-1])
+        x_true[:,k] = np.dot(F,x_true[:,k-1])
         x_true[:,k] = randomize_direction(x_true[:,k]).reshape(4)
-        x_est_prior[:,k] = np.matmul(F,x_est_posterior[:,k-1])
-        cov_prior[:,:,k] = np.matmul(np.matmul(F,cov_posterior[:,:,k-1]),np.transpose(F))+Q
+        x_est_prior[:,k] = np.dot(F,x_est_posterior[:,k-1])
+        cov_prior[:,:,k] = np.dot(np.dot(F,cov_posterior[:,:,k-1]),F.T)+Q
     
     # Generate measurement for real target
-    noise = np.matmul(np.linalg.cholesky(R),np.random.normal(size=[2,1]))
-    z[:,k] = np.matmul(H,np.transpose(x_true[:,k]))+np.transpose(noise)
+    noise = np.dot(np.linalg.cholesky(R),np.random.normal(size=[2,1]))
+    z_true[:,k] = np.dot(H,x_true[:,k])+noise.T
     
     # Add clutter measurements and signal strength
     lambda1 = clut_l + (clut_h - clut_l)*random.random()   # Clutter density
     num_clut = math.floor(lambda1*V)              # Number of clutter points
     
-    z_all = np.array([np.append(z[0,k], np.random.randint(low=x_lim[0], high=x_lim[1],size=num_clut)),
-        np.append(z[1,k], np.random.randint(low=y_lim[0], high=y_lim[1],size=num_clut)),
+    z_all = np.array([np.append(z_true[0,k], np.random.randint(low=x_lim[0], high=x_lim[1],size=num_clut)),
+        np.append(z_true[1,k], np.random.randint(low=y_lim[0], high=y_lim[1],size=num_clut)),
         np.random.random(num_clut+1)])
     
     # Test signal strength
@@ -105,24 +107,24 @@ for k in range(K):
             z_strength = np.append(z_strength, [z_all[0:2,i]], axis=0)
 
     # Find measurements within validation region
-    S = np.matmul(np.matmul(H,cov_prior[:,:,k]),np.transpose(H))+R           # Covariance of the innovation
-    W = np.matmul(cov_prior[:,:,k],np.transpose(H))             # Gain
-    W = np.matmul(W,np.linalg.inv(S))
+    S = np.dot(np.dot(H,cov_prior[:,:,k]),H.T)+R           # Covariance of the innovation
+    W = np.dot(cov_prior[:,:,k],H.T)             # Gain
+    W = np.dot(W,np.linalg.inv(S))
     m_k = 0
     beta_i = np.array([0])
     v_i = np.array([[0], [0]])
-    for i in range(len(z_strength[0,:])):
-        v_ik = z_strength[i] - np.matmul(H,x_est_prior[:,k])       # Measurement innovation
-        NIS_temp = np.matmul(np.transpose(v_ik),np.linalg.inv(S))
-        NIS_temp = np.matmul(NIS_temp,v_ik)
+    for i in range(len(z_strength[:,0])):
+        v_ik = z_strength[i] - np.dot(H,x_est_prior[:,k])       # Measurement innovation
+        #NIS_temp = np.dot(v_ik.T,np.linalg.inv(S)).dot(v_ik)
+        NIS_temp = np.dot(np.dot(v_ik.T, np.linalg.inv(S)),v_ik)
         if NIS_temp < gamma_g:                           # Within validation region
-            #z_gate = [z_gate [z_strength(i).x k]]
-            np.append(v_i, v_ik, axis=0)
-            np.append(beta_i, exp(-0.5*NIS_temp))
+            z_gate = np.append(z_gate, [[z_strength[i,0]], [z_strength[i,1]]], axis=1)
+            v_i = np.append(v_i, [[v_ik[0]], [v_ik[1]]], axis=1)
+            beta_i = np.append(beta_i, np.exp(-0.5*NIS_temp))
             m_k += 1
 
     beta_i[0] = 2*(1-PD*PG)*m_k/(gamma_g)
-    if beta_i != 0:
+    if beta_i[0] != 0:
         beta_i = beta_i/np.sum(beta_i)                  # Normalize
     
     if m_k != 0:
@@ -132,13 +134,29 @@ for k in range(K):
         v_k = np.array([[0], [0]])
     
     # Covariance of correct measurement and SOI
-    P_c = cov_prior[:,:,k]-np.matmul(np.matmul(W,S),np.transpose(W))
+    P_c = cov_prior[:,:,k]-np.dot(np.dot(W,S),W.T)
     part_result = 0
     for i in range(1,len(beta_i)):
-        part_result = part_result + beta_i[i]*(v_i[:,i]*np.transpose(v_i[:,i]))
-    part_result -= np.matmul(v_k,np.transpose(v_k))
-    SOI = np.matmul(np.matmul(W,part_result),np.transpose(W))
+        temp = v_i[:,i][np.newaxis]
+        part_result = part_result + beta_i[i]*(temp.T*temp)
+    part_result -= np.dot(v_k,v_k.T)
+    SOI = np.dot(np.dot(W,part_result),W.T)
 
-    temp = np.transpose([x_est_prior[:,k]])+np.matmul(W,v_k)
+    temp = np.transpose([x_est_prior[:,k]])+np.dot(W,v_k)
     x_est_posterior[:,k] = temp.reshape(4)
     cov_posterior[:,:,k] = beta_i[0]*cov_prior[:,:,k]+(1-beta_i[0])*P_c+SOI
+
+# -----------------------------------------------
+
+plt.plot(x_true[2,:], x_true[0,:],'r', label='True')
+plt.plot(z_true[1,:], z_true[0,:], 'o', fillstyle='none', markeredgecolor='xkcd:wheat', label='data1')
+plt.plot(z_gate[1,:], z_gate[0,:], 'kx', label='data within gate')
+plt.plot(x_est_posterior[2,:], x_est_posterior[0,:], 'b--', label='Posterior', linewidth=2)
+
+plt.xlabel('East[m]')
+plt.ylabel('North[m]')
+plt.title('Track position with sample rate: 1/s')
+plt.legend()
+
+
+plt.show()
